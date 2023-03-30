0735e83c38a0e27530c597114fd2228c31f73e3e
강창래 <crkang@Covision.local>
2023-03-30 11:28:38 +0900

    :wrench: Screens 추가

---
+++ src/screens/EdittProfile.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const EdittProfile = () => {
+  return (
+    <View>
+      <Text>EdittProfile</Text>
+    </View>
+  );
+};
+
+export default EdittProfile;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/FreindProfile.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const FreindProfile = () => {
+  return (
+    <View>
+      <Text>FreindProfile</Text>
+    </View>
+  );
+};
+
+export default FreindProfile;
+
+const styles = StyleSheet.create({});
